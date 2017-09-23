201ff352e720e457fc105dd4662920a3d3836568
Carlos Luna <>
2017-09-22 20:23:44 -0600

    Proyecto 1 - Commit 1

---
+++ src/proyecto1_201602491/Proyecto1_201602491.java
@@ -0,0 +1,16 @@
+package proyecto1_201602491;
+
+/**
+ *
+ * @author Carlos Luna 
+ */
+public class Proyecto1_201602491 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MenuP menup = new MenuP();
+    }
+    
+}
